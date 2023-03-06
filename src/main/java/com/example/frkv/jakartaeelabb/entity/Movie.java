2f345ea81404e793771546d9ba836f656798494b
Fredrik Kvarnåsen <102403209+figgge@users.noreply.github.com>
2023-03-06 10:48:44 +0100

    Add movie entity class

---
+++ src/main/java/com/example/frkv/jakartaeelabb/entity/Movie.java
@@ -0,0 +1,35 @@
+package com.example.frkv.jakartaeelabb.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotNull
+    @Size(min = 1)
+    private String name;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
